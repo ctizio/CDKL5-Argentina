1ef2ea378136417d40c92d64cfc20261d4c31d7e
Cristina Tizio <cristizio@yahoo.com.ar>
2022-09-30 07:58:48 -0300

    Adaptacion efecto Carrousel a galeria de imagenes en pagina nosotros.html, nosotros.css y script.js

--- js/script.js
+++ js/script.js
@@ -24,3 +24,17 @@ navWrapper.addEventListener("click", e => {
     toggleButton.classList.remove("close");
   }
 });
+
+document.addEventListener('DOMContentLoaded',()=>{
+  const elementosCarousel=document.querySelectorAll('.carousel');
+  M.Carousel.init(elementosCarousel,{
+    duration:150,
+    dist:-80,
+    shift:5,
+    padding:5,
+    numVisible:4,
+    indicators:true,
+    nowrap:true
+
+  }) 
+})
