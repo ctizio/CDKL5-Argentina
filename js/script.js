116b45af6cf2b4e1a9831fd4628bebecb749d49e
Cristina Tizio <cristizio@yahoo.com.ar>
2022-10-12 11:24:36 -0300

    Adecuacion imagenes a array de objetos familias en script.js

--- js/script.js
+++ js/script.js
@@ -59,18 +59,21 @@ let familias = [
   {
     nombre: "Catalina Genen",
     foto: "img/foto6.png",
+    foto2:"img/foto7.png",
     alt: "foto6",
     descripcion: "Actualmente de 10 años de la provincia de Neuquén. A los 3 años fue diagnosticada con el síndrome CDKL5, pero los síntomas de esta enfermedad comenzaron cuando apenas nació, ya que fue una bebe que lloraba continuamente y dormía muy poco. Al mes y medio de vida comenzaron las convulsiones que hasta el día de hoy persisten diariamente. Puede caminar con ayuda, tiene alteraciones sensoriales, problemas de deglución, no puede hablar, tienen convulsiones todos los días, es hiperlaxa, tiene problemas de sueño, le molesta el ruido y sufre de problemas intestinales y estomacales, entre otras tantas dolencias… Ha vivido situaciones difíciles en su corta edad, pero se ha sobrepuesto a todo. Tiene 10 años y en este tiempo aprendió a sonreír, a bailar, a andar en bici. Es  fanática de los paseos en auto, le encantan las hamacas y los árboles. Su familia anhela un tratamiento y una cura."
   },
   {
     nombre: "Allegra",
     foto: "img/foto8.png",
+    foto2:"img/foto11.png",
     alt: "foto8",
     descripcion: "Ella es Allegra, segunda hija que actualmente tiene 21 meses. De la provincia de Córdoba.Allegra fue diagnosticada Con el síndrome de deficiencia de CDKL5 cuando tenía 15 meses, luego de realizar un estudio genético. A los dos meses de vida comenzó con convulsiones. Requirió internaciones para realizar estudios y hasta el día de hoy continúa con crisis, aun así estando medicada. Alegra asiste a tres tipos de terapias 8 veces por semana que la ayudan a desarrollarse y en casa su familia ayuda para ello. Su familia anhela que los expertos puedan investigar más y tener la cura."
   },
   {
     nombre: "Jimena",
     foto: "img/foto3.png",
+    foto2:"img/foto9.png",
     alt: "foto3",
     descripcion: "Ella es Jimena de 4 años de la provincia de Córdoba. El síndrome CDKL5 inició a los 6 meses de vida con convulsiones mientras dormía y ante la preocupación sus padres acudieron a la guardia médica y ahí comenzó su internación para determinar el origen de las crisis. Entre ellos el estudio genético que confirmaría meses después la enfermedad CDKL5. Como no se conoce mucho sobre esta enfermedad rara, su familia espera que se visibilicen los casos para tener mayor conocimiento. Su familia abraza fuerte a todas las familias que tienen un niño con CDKL5 y que con fe y esperanza esperan la cura."
   }
@@ -86,6 +89,7 @@ for (let persona of familias) {
   nom.innerText = persona.nombre;
   //agrego h3 dentro de div .descripcion-nombre
   descNos.appendChild(nom);
+
   //cre div class="fotos"
   let descFoto = document.createElement("div");
   descFoto.className = "fotos";
@@ -98,6 +102,16 @@ for (let persona of familias) {
   descImg.id = "fotoX1";
   //agrego img #fotoX1 a div .fotos;
   descFoto.appendChild(descImg);
+ // creo img
+ let descImg2 = document.createElement("img");
+ descImg2.src= persona.foto2;
+ descImg2.alt= persona.alt;
+ descImg2.id = "fotoX1";
+ //agrego img #fotoX1 a div .fotos;
+ descFoto.appendChild(descImg2);
+
+
+
   // creo div class="descripcion-parrafo"
   let descDesc = document.createElement("div");
   descDesc.className = "descripcion-nosotros";
